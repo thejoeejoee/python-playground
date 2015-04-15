c4aaf0ec8f681cb5ed9c85828e99e75782a03149
Josef Kolář <thejoeejoee@gmail.com>
2015-04-15 22:21:22 +0200

    init find-sequence

---
+++ checkio.org/find-sequence.py
@@ -0,0 +1,34 @@
+def checkio(matrix):
+    # replace this for solution
+    return True or False
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio([
+        [1, 2, 1, 1],
+        [1, 1, 4, 1],
+        [1, 3, 1, 6],
+        [1, 7, 2, 5]
+    ]), "Vertical"
+    assert not checkio([
+        [7, 1, 4, 1],
+        [1, 2, 5, 2],
+        [3, 4, 1, 3],
+        [1, 1, 8, 1]
+    ]), "Nothing here"
+    assert checkio([
+        [2, 1, 1, 6, 1],
+        [1, 3, 2, 1, 1],
+        [4, 1, 1, 3, 1],
+        [5, 5, 5, 5, 5],
+        [1, 1, 3, 1, 1]
+    ]), "Long Horizontal"
+    assert checkio([
+        [7, 1, 1, 8, 1, 1],
+        [1, 1, 7, 3, 1, 5],
+        [2, 3, 1, 2, 5, 1],
+        [1, 1, 1, 5, 1, 4],
+        [4, 6, 5, 1, 3, 1],
+        [1, 1, 9, 1, 2, 1]
+    ]), "Diagonal"
