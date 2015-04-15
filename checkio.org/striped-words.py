146a7324fd3b81383fd6c1d0682c7e17bf814218
Josef Kolář <thejoeejoee@gmail.com>
2015-04-15 21:13:12 +0200

    commented regex

--- checkio.org/striped-words.py
+++ checkio.org/striped-words.py
@@ -16,9 +16,17 @@ def checkio(text):
 
 def is_word_ok(word):
     pattern = r"""
-    ^(([BCDFGHJKLMNPQRSTVWXZ]{1})([AEIOUY]{1}))*([BCDFGHJKLMNPQRSTVWXZ]?)$
-    |
-    ^(([AEIOUY]{1})([BCDFGHJKLMNPQRSTVWXZ]{1}))*([AEIOUY]?)$
+    ^(
+        ([BCDFGHJKLMNPQRSTVWXZ]{1})([AEIOUY]{1})    # couple of CONSONANT and VOWEL
+    )*                                              # unlimited occurrences
+    ([BCDFGHJKLMNPQRSTVWXZ]?                        # optionally followed by trailing single VOWEL
+    )$
+    |                                               # or
+    ^(
+        ([AEIOUY]{1})([BCDFGHJKLMNPQRSTVWXZ]{1})    # couple of VOWEL and CONSONANT
+    )*                                              # unlimited occurrences
+    ([AEIOUY]?                                      # optionally followed by trailing single CONSONANT
+    )$
     """
     if len(word) > 1 and match(pattern, word, I + M + S + X):
         return True
@@ -30,5 +38,5 @@ if __name__ == '__main__':
     assert checkio(u"Hello world") == 0, "No one"
     assert checkio(u"A quantity of striped words.") == 1, "Only of"
     assert checkio(u"Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"
-    assert checkio(
-        u"To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?") == 8, 'muhaha'
+    assert checkio(u"To take a trivial example, which of us ever undertakes laborious physical exercise,"
+                   u"except to obtain some advantage from it?") == 8, 'it?'
